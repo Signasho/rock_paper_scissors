77af3d3ed3398c5fa1d5e6cdae5e85f85b0ac790
Navarroseb <sebastnavarro@gmail.com>
2024-02-16 17:39:54 -0300

    Starting project

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+let gameshtml = document.getElementById("games");
+//let gamesprompt = +prompt("Ingrese la cantidad de veces que deseas jugar:");
+
+//gameshtml.innerHTML = gamesprompt;
+
+let rockButton = document.getElementById("rock");
+let paperButton = document.getElementById("paper");
+let scissorsButton = document.getElementById("scissors");
+
+rockButton = 0;
+paperButton = 1;
+scissorsButton = 2;
+console.log(rockButton, paperButton, scissorsButton)
+
+const userPlay = () => {
+    
+
+}
+
+
+
