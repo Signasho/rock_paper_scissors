060dd4dd45312c5a25b05e1250aa974c2b2354d7
Navarroseb <sebastnavarro@gmail.com>
2024-02-17 16:16:13 -0300

    logic ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,57 @@
-let gameshtml = document.getElementById("games");
-//let gamesprompt = +prompt("Ingrese la cantidad de veces que deseas jugar:");
+let userPlay = document.getElementById('userSelection')
+let rockIcon = `<i class="fa-regular fa-hand-back-fist fa-2xl"></i>`;
+let paperIcon = `<i class="fa-regular fa-hand fa-2xl"></i>`;
+let scissorsIcon = `<i class="fa-regular fa-hand-scissors fa-2xl"></i>`;
+let machinePlay = document.getElementById('machineGame')
+let resultGame = document.getElementById('result')
 
-//gameshtml.innerHTML = gamesprompt;
 
-let rockButton = document.getElementById("rock");
-let paperButton = document.getElementById("paper");
-let scissorsButton = document.getElementById("scissors");
+const choices = [rockIcon, paperIcon, scissorsIcon];
 
-rockButton = 0;
-paperButton = 1;
-scissorsButton = 2;
-console.log(rockButton, paperButton, scissorsButton)
 
-const userPlay = () => {
+
+    //let games = prompt("Ingresa la cantidad de veces que deseas jugar!");
+
+
+        const playGame = (userGame) => {
+            const machineGame = choices[Math.floor(Math.random() * 3)];
+            let result = "";
+
+            if (userGame === 'rock') {
+                userGame = rockIcon;
+            }
+            if (userGame === 'paper') {
+                userGame = paperIcon
+            }
+            if (userGame === 'scissors') {
+                userGame = scissorsIcon
+            }
+
+            if (userGame === machineGame) {
+                result = "EMPATE";
+            }
+            else {
+                switch (userGame) {
+                    case rockIcon:
+                        result = (machineGame === scissorsIcon) ? "GANASTE!" : "PERDISTE!";
+                        break;
+                    case paperIcon:
+                        result = (machineGame === rockIcon) ? "GANASTE!" : "PERDISTE!";
+                        break;
+                    case scissorsIcon:
+                        result = (machineGame === paperIcon) ? "GANASTE!" : "PERDISTE!";
+                        break;
+                }
+            }
+
+            userPlay.innerHTML = userGame;
+            machinePlay.innerHTML = machineGame;
+            resultGame.textContent = result;
+        }
     
 
-}
+
+
 
 
 
